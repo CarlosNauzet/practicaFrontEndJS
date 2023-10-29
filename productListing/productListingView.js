7642e50c9ed342289aeb18cb8b96fdb84587a0ef
Carlos Nauzet Hernández <carlos.hernandez@motork.io>
2023-10-29 09:21:50 +0100

    Arreglo error carga imágenes

--- productListing/productListingView.js
+++ productListing/productListingView.js
@@ -1,7 +1,7 @@
 export const renderProduct = (product) => {
   return `
   <a href="./product-detail.html?id=${product.id}" class="product-links">
-    <img scr="${product.image}" alt="product-image" onerror="this.src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png'" />
+    <img src="${product.image}" alt="product-image" onerror="this.src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png'" />
     <p class="namestyle">Name: ${product.name}</p>
     <p class="namestyle">Description: ${product.description}</p>
     <p class="price-state">Price: ${product.price}</p>
